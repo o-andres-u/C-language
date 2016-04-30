1bdab1483e5632aaa4d8db7a061bd724a3ad7756
o-andres-u <oscarandresu@gmail.com>
2016-04-29 22:31:04 -0500

    Sample creating a thread

---
+++ Lab4/creacion_hilo.c
@@ -0,0 +1,20 @@
+#include <pthread.h>
+#include <stdio.h>
+
+/* Imprime 'x' en la salida de error stderr.
+El parámetro no es usado. No tiene retorno */
+void* imprime_x (void* unused) {
+  while (1) {
+    fputc('x', stderr);
+    return NULL;
+  }
+}
+
+int main() {
+  pthread_t id_hilo;
+  /* Crea un nuevo hilo sobre la función imprime_x */
+  pthread_create(&id_hilo, NULL, &imprime_x, NULL);
+  /* Imprime 'o' en la salida de error stderr */
+  while (1) fputc('o', stderr);
+  return 0;
+}
