f72f6e6c2afbd348eab8f545005f4cf5d01bcb03
o-andres-u <oscarandresu@gmail.com>
2016-05-17 13:03:59 -0500

    Race condicion example

---
+++ Lab5/race_condition.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define NUMTHREADS 200
+#define MAXCNT 1000
+
+double counter = 0;
+void* counting(void *);
+
+int main(void) {
+  pthread_t tid[NUMTHREADS];
+  int i;
+
+  for (i = 0; i < NUMTHREADS; i++) {
+    pthread_create(&tid[i], NULL, &counting, NULL);
+  }
+
+  for (i = 0; i < NUMTHREADS; i++) {
+    pthread_join(tid[i], NULL);
+  }
+
+  printf("\nCounter must be in: %d\n", MAXCNT * NUMTHREADS);
+  printf("\nCounter value is: %.0f\n\n", counter);
+
+  return 0;
+}
+
+void* counting(void *unused) {
+  int i;
+  for (i = 0; i < MAXCNT; i++) {
+    counter++;
+  }
+  return NULL;
+}
